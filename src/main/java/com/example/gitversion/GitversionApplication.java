23aa5cc52d1d7e6efe8f0011c4dc154b70bfdd6b
14377 <likangshuai@wahaha.com.cn>
2019-12-30 15:21:20 +0800

    version2

---
+++ src/main/java/com/example/gitversion/GitversionApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitversionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitversion/GitversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitversionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
